daeb04c5e51c2c016fb4de5189a179d27372f113
Kamil Tekiela <tekiela246@gmail.com>
2024-11-05 21:32:34 +0000

    Fix type hint
    Signed-off-by: Kamil Tekiela <tekiela246@gmail.com>

--- src/Display/Results.php
+++ src/Display/Results.php
@@ -853,14 +853,14 @@ class Results
      *
      * @see getTableHeaders()
      *
-     * @param mixed[]|null $sortExpression   the sort expression
-     * @param string       $unsortedSqlQuery the unsorted sql query
+     * @param string[] $sortExpression   the sort expression
+     * @param string   $unsortedSqlQuery the unsorted sql query
      *
      * @return mixed[][]
      * @psalm-return array{hidden_fields?:array, options?:array}
      */
     private function getSortByKeyDropDown(
-        array|null $sortExpression,
+        array $sortExpression,
         string $unsortedSqlQuery,
     ): array {
         // grab indexes data:
@@ -884,7 +884,7 @@ class Results
         }
 
         $isIndexUsed = false;
-        $localOrder = is_array($sortExpression) ? implode(', ', $sortExpression) : '';
+        $localOrder = implode(', ', $sortExpression);
 
         $options = [];
         foreach ($indexes as $index) {
